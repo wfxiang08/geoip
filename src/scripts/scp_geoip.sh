06b0206a0bdf5665c4ea8b9c3c131c843f1d0453
fei.wang <fei.wang@ushow.media>
2017-07-18 14:30:31 +0800

    重新整理rpc的异常处理

--- src/scripts/scp_geoip.sh
+++ src/scripts/scp_geoip.sh
@@ -7,8 +7,8 @@ fi
 host_name=$1
 
 ssh root@${host_name} "mkdir -p /usr/local/services/geoip/"
-ssh root@${host_name} "chown worker.worker -r /usr/local/services/geoip/"
-
 scp config_standalone.ini  root@${host_name}:/usr/local/services/geoip/config_standalone.ini
 ssh root@${host_name} "rm -f /usr/local/services/geoip/service_geoip"
 scp service_geoip root@${host_name}:/usr/local/services/geoip/service_geoip
+
+ssh root@${host_name} "chown -R worker.worker /usr/local/services/geoip/"
